bddc152062f428fc52a5d627d2bd17896bdaccf4
FelipeLL <f3lip3.3z@hotmail.com>
2022-08-12 17:50:28 -0500

    Add a Home component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,327 @@
 import styles from "../styles/home.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGrip } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSackDollar,
+  faCircleDollarToSlot,
+} from "@fortawesome/free-solid-svg-icons";
 const Home = () => {
   return (
     <div className={styles["home-content"]}>
-      <p>hola</p>
+      <div className={styles["container-1"]}>
+        <div className={styles.cards}>
+          <div
+            className={`${styles.card} ${styles["border-left-primary"]} ${styles.shadow} ${styles["h-100"]}  `}
+          >
+            <div className={styles["card-body"]}>
+              <div>
+                <div className={styles["card-title"]}>Total income</div>
+                <p className={styles["card-text"]}>$40,000</p>
+              </div>
+              <div className={styles["card-icon"]}>
+                <FontAwesomeIcon icon={faSackDollar} />
+              </div>
+            </div>
+          </div>
+          <div
+            className={`${styles.card} ${styles["border-left-primary"]} ${styles.shadow} ${styles["h-100"]} `}
+          >
+            <div className={styles["card-body"]}>
+              <div>
+                <div className={styles["card-title"]}>Total expense</div>
+                <p className={styles["card-text"]}>$30,000</p>
+              </div>
+              <div className={styles["card-icon"]}>
+                <FontAwesomeIcon icon={faCircleDollarToSlot} />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className={`${styles.circular} ${styles["circular-card"]}  ${styles.shadow}   `}
+        >
+          <div className={styles["circular-title"]}>Current balance</div>
+          <div className={styles["circular-progress"]}>
+            <span className={styles["progress-value"]}>
+              {" "}
+              <span>+</span> 29.50000
+            </span>
+          </div>
+
+          <span className={styles["circular-text"]}>I & E</span>
+        </div>
+      </div>
+
+      <div className={styles["container-2"]}>
+        <ul className={styles["responsive-table"]}>
+          <li className={styles["table-header"]}>
+            <div className={`${styles["col"]} ${styles["col-1"]}`}>ID</div>
+            <div className={`${styles["col"]} ${styles["col-2"]}`}>DATE</div>
+            <div className={`${styles["col"]} ${styles["col-3"]}`}>TYPE</div>
+            <div className={`${styles["col"]} ${styles["col-4"]}`}>AMOUNT</div>
+          </li>
+          <li className={styles["table-row"]}>
+            <div
+              className={`${styles["col"]} ${styles["col-1"]}`}
+              data-label="ID"
+            >
+              42
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-2"]}`}
+              data-label="Date"
+            >
+              10/08/2022
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-3"]}`}
+              data-label="Type"
+            >
+              Income
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-4"]}`}
+              data-label="Amount"
+            >
+              $350
+            </div>
+          </li>
+          <li className={styles["table-row"]}>
+            <div
+              className={`${styles["col"]} ${styles["col-1"]}`}
+              data-label="ID"
+            >
+              42
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-2"]}`}
+              data-label="Date"
+            >
+              10/08/2022
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-3"]}`}
+              data-label="Type"
+            >
+              Income
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-4"]}`}
+              data-label="Amount"
+            >
+              $350
+            </div>
+          </li>
+          <li className={styles["table-row"]}>
+            <div
+              className={`${styles["col"]} ${styles["col-1"]}`}
+              data-label="ID"
+            >
+              42
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-2"]}`}
+              data-label="Date"
+            >
+              10/08/2022
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-3"]}`}
+              data-label="Type"
+            >
+              Income
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-4"]}`}
+              data-label="Amount"
+            >
+              $350
+            </div>
+          </li>
+          <li className={styles["table-row"]}>
+            <div
+              className={`${styles["col"]} ${styles["col-1"]}`}
+              data-label="ID"
+            >
+              42
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-2"]}`}
+              data-label="Date"
+            >
+              10/08/2022
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-3"]}`}
+              data-label="Type"
+            >
+              Income
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-4"]}`}
+              data-label="Amount"
+            >
+              $350
+            </div>
+          </li>
+          <li className={styles["table-row"]}>
+            <div
+              className={`${styles["col"]} ${styles["col-1"]}`}
+              data-label="ID"
+            >
+              42
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-2"]}`}
+              data-label="Date"
+            >
+              10/08/2022
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-3"]}`}
+              data-label="Type"
+            >
+              Income
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-4"]}`}
+              data-label="Amount"
+            >
+              $350
+            </div>
+          </li>
+          <li className={styles["table-row"]}>
+            <div
+              className={`${styles["col"]} ${styles["col-1"]}`}
+              data-label="ID"
+            >
+              42
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-2"]}`}
+              data-label="Date"
+            >
+              10/08/2022
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-3"]}`}
+              data-label="Type"
+            >
+              Income
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-4"]}`}
+              data-label="Amount"
+            >
+              $350
+            </div>
+          </li>
+          <li className={styles["table-row"]}>
+            <div
+              className={`${styles["col"]} ${styles["col-1"]}`}
+              data-label="ID"
+            >
+              42
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-2"]}`}
+              data-label="Date"
+            >
+              10/08/2022
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-3"]}`}
+              data-label="Type"
+            >
+              Income
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-4"]}`}
+              data-label="Amount"
+            >
+              $350
+            </div>
+          </li>
+          <li className={styles["table-row"]}>
+            <div
+              className={`${styles["col"]} ${styles["col-1"]}`}
+              data-label="ID"
+            >
+              42
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-2"]}`}
+              data-label="Date"
+            >
+              10/08/2022
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-3"]}`}
+              data-label="Type"
+            >
+              Income
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-4"]}`}
+              data-label="Amount"
+            >
+              $350
+            </div>
+          </li>
+          <li className={styles["table-row"]}>
+            <div
+              className={`${styles["col"]} ${styles["col-1"]}`}
+              data-label="ID"
+            >
+              42
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-2"]}`}
+              data-label="Date"
+            >
+              10/08/2022
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-3"]}`}
+              data-label="Type"
+            >
+              Income
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-4"]}`}
+              data-label="Amount"
+            >
+              $350
+            </div>
+          </li>
+          <li className={styles["table-row"]}>
+            <div
+              className={`${styles["col"]} ${styles["col-1"]}`}
+              data-label="ID"
+            >
+              42
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-2"]}`}
+              data-label="Date"
+            >
+              10/08/2022
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-3"]}`}
+              data-label="Type"
+            >
+              Income
+            </div>
+            <div
+              className={`${styles["col"]} ${styles["col-4"]}`}
+              data-label="Amount"
+            >
+              $350
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
